ae9cc9e12a465ac4159f8e226f9410347e33dc2d
Anam Aadel <aadelbanat8991@gmail.com>
2024-03-21 23:32:20 +0600

    completed backend functionality

---
+++ index.js
@@ -0,0 +1,76 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+var cookieParser = require("cookie-parser");
+require("dotenv").config();
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
+
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+  },
+});
+
+const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.otazdf5.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+const db = client.db("myShopDB");
+const conversationCollection = db.collection("conversation");
+
+app.get("/", async (req, res) => {
+  res.send("server is running");
+});
+
+app.get("/get_conversation", async (req, res) => {
+  const conversation = await conversationCollection.find().toArray();
+  res.status(200).send(conversation);
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("message", async (data) => {
+    try {
+      const result = await conversationCollection.insertOne(data);
+
+      if (result.acknowledged) {
+        const conversation = await conversationCollection.find().toArray();
+        socket.emit("conversation", conversation);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`server listening on ${port}`);
+});
